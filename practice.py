b62529f47c9f0a3855a13dcf0cf2090927d0bb42
Demian <52223640+sath72@users.noreply.github.com>
2024-06-05 12:32:46 -0700

    Add files via upload
    add practice.py

---
+++ practice.py
@@ -0,0 +1,29 @@
+print(5)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*8)
+print(3*3+1)
+print('baloon')
+print("key"*9)
+print(5<10)
+print(True)
+print(not False)
+print(not True)
+print(not 5>10)
+#Please introduce your pet
+animal = "Dog"
+name = "Rivvi"
+age = 1
+hobby = "stroll"
+is_adult = age >=3
+print("My " + animal + " is " + name)
+#print(name + " is " + str(age) + " year old")
+''' comments area
+and
+and
+
+'''
+print(name,"is",age,"year old")
+print(name + " is cute and small & " + " Hobby is "+hobby)
+print(name + " is adult ? "+ str(is_adult))
