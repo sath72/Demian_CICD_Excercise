ff5324deb3b1fd9aa061faa253b35faa90a4fd2b
Demian <52223640+sath72@users.noreply.github.com>
2024-06-05 12:29:00 -0700

    Add files via upload
    add Python Test 7.py

---
+++ Python Test 7.py
@@ -0,0 +1,34 @@
+'''
+num=int(input('Please Input Number : '))
+if num%2==0:
+    print('{} is Even number.'.format(num))
+else :
+    print('{} is Even number.'.format(num))
+'''
+'''
+# print('if a is >=0, keep working otherwise stop : ')
+
+a=int(input('a : '))
+'''
+'''
+while a>=0:
+      print('Execution')
+      a=int(input('a : '))
+
+
+while a:
+    print(a)
+    a=a-1
+'''
+
+text=input('e or E will end : ')
+
+while text!='e' and text!='E':
+    text=input('e or E will end : ')
+
+
+
+            
+
+
+    
